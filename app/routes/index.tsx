c34cd4ff86fdb78e43e7618de7eac4b6a913722e
niuchunlei <niuchunlei@mokahr.com>
2025-12-28 14:43:15 +0800

    feat: 删除无用代码

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,11 +1,4 @@
-import { HomeCollaborateSection } from "~/components/Home/HomeCollaborateSection";
-import { HomeEdgeCasesSection } from "~/components/Home/HomeEdgeCasesSection";
-import { HomeFeatureGridSection } from "~/components/Home/HomeFeatureGridSection";
-import { HomeHeader } from "~/components/Home/HomeHeader";
 import { HomeHeroSection } from "~/components/Home/HomeHeroSection";
-import { HomeInfoBoxSection } from "~/components/Home/HomeInfoBoxSection";
-import { HomeSearchSection } from "~/components/Home/HomeSearchSection";
-import { HomeFooter } from "~/components/Home/HomeFooter";
 import {
   commitSession,
   getSession,
